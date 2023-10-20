37244012b746880ba5d9381a77dd2510f8048ba1
Vijayakumar92 <vijayakumarnigadi@gmail.com>
2023-10-20 09:30:34 +0530

    Service class commit

--- src/main/java/com/cg/service/SeriesService.java
+++ src/main/java/com/cg/service/SeriesService.java
@@ -6,13 +6,30 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus.Series;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cg.dao.SeriesDao;
 import com.cg.repository.SeriesRepository;
 
 @Service
-public class SeriesService {
+public class SeriesService implements SeriesDao {
 
 	@Autowired
 	private SeriesRepository srepository;
+	
+	List<Series> lseries=new ArrayList<>();
+	
+	
+	public String addSeriesName(Series name) {
+		lseries.add(name);
+		return "Series added successfully";
+		
+	}
+
+	public List<Series> getAllSerieses() {
+	
+		return lseries;
+	}
+	
 	
 }
