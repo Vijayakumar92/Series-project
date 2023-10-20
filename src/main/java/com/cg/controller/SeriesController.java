30502411cfb496013ccff01fdba5ec0b96340c76
Vijayakumar92 <vijayakumarnigadi@gmail.com>
2023-10-20 09:42:15 +0530

    Controller class commit

--- src/main/java/com/cg/controller/SeriesController.java
+++ src/main/java/com/cg/controller/SeriesController.java
@@ -1,6 +1,12 @@
 package com.cg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus.Series;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.service.SeriesService;
@@ -10,5 +16,17 @@ public class SeriesController {
 	
 	@Autowired
 	private SeriesService sservice;
+	
+	@PostMapping("/add")
+	public String addSeries(@RequestBody Series name) {
+		return sservice.addSeriesName(name);
+		
+	}
+	
+	public List<Series> gettAll(){
+		return sservice.getAllSerieses();
+		
+	}
+	
 
 }
